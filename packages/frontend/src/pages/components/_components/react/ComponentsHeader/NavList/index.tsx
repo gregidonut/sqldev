99ac54d57c727edb93f0a0e17c6232e86c24e107
Greg Osilaja <gregosilaja@outlook.com>
2026-04-26 22:51:21 +0800

    feat: consolidate the component pages decorations
    So i can define the glob loader for the paths in a single location

--- packages/frontend/src/pages/components/_components/react/ComponentsHeader/NavList/index.tsx
+++ packages/frontend/src/pages/components/_components/react/ComponentsHeader/NavList/index.tsx
@@ -1,10 +1,16 @@
-import {Autocomplete, useFilter} from 'react-aria-components';
-import {MenuTrigger, Menu, MenuItem} from './Menu/Menu.tsx';
-import {Button} from './Menu/Button.tsx';
-import {SearchField} from './Menu/SearchField.tsx';
+import { Autocomplete, useFilter } from "react-aria-components";
+import { MenuTrigger, Menu, MenuItem } from "./Menu/Menu.tsx";
+import { Button } from "./Menu/Button.tsx";
+import { SearchField } from "./Menu/SearchField.tsx";
 
-export default function Index({pathname}: { pathname: string }) {
-    let {contains} = useFilter({sensitivity: 'base'});
+export default function Index({
+    pathname,
+    staticpaths,
+}: {
+    pathname: string;
+    staticpaths: string[];
+}) {
+    let { contains } = useFilter({ sensitivity: "base" });
 
     return (
         <nav>
@@ -12,17 +18,32 @@ export default function Index({pathname}: { pathname: string }) {
                 <h2>
                     <Button>{pathname}</Button>
                 </h2>
-                <div style={{display: 'flex', flexDirection: 'column', maxHeight: 'inherit'}}>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        maxHeight: "inherit",
+                    }}
+                >
                     <Autocomplete filter={contains}>
-                        <SearchField aria-label="Search Components" placeholder="Search Components" autoFocus
-                                     style={{margin: 4}}/>
-                        <Menu style={{flex: 1}}>
-                            <MenuItem href="/components/basicForm"
-                                      isDisabled={pathname === 'basicForm'}
-                            >basicForm</MenuItem>
-                            <MenuItem href="/components/basicFileUpload"
-                                      isDisabled={pathname === 'basicFileUpload'}>
-                                basicFileUpload</MenuItem>
+                        <SearchField
+                            aria-label="Search Components"
+                            placeholder="Search Components"
+                            autoFocus
+                            style={{ margin: 4 }}
+                        />
+                        <Menu style={{ flex: 1 }}>
+                            {staticpaths.map(function (path, index) {
+                                return (
+                                    <MenuItem
+                                        href={`/components/${path}`}
+                                        isDisabled={pathname === path}
+                                        key={index}
+                                    >
+                                        {path}
+                                    </MenuItem>
+                                );
+                            })}
                         </Menu>
                     </Autocomplete>
                 </div>
--- packages/frontend/src/pages/components/_components/react/ComponentsHeader/index.tsx
+++ packages/frontend/src/pages/components/_components/react/ComponentsHeader/index.tsx
@@ -4,13 +4,22 @@ import { CurrentPageContext } from "./CurrentPageContext.ts";
 
 const NavList = lazy(() => import("./NavList"));
 
-export default function ComponentsHeader({ pathname }: { pathname: string }) {
+export default function ComponentsHeader({
+    pathname,
+    staticpaths,
+}: {
+    pathname: string;
+    staticpaths: string[];
+}) {
     return (
         <header className="flex-row-start">
             <nav className="flex-row-start gap-3 text-[0.7rem] ">
                 <Suspense fallback={<div>Loading...</div>}>
                     <CurrentPageContext.Provider value={pathname}>
-                        <NavList pathname={pathname} />
+                        <NavList
+                            pathname={pathname}
+                            staticpaths={staticpaths}
+                        />
                     </CurrentPageContext.Provider>
                 </Suspense>
             </nav>
